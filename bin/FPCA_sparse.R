a4ac8635142a9aeb1cf48a9be03fba140a1b7104
MCouteyen <m.couteyen_carpaye@ecologie.re>
2022-05-02 16:13:27 +0200

    Updata FPCA and first commit of sparse FPCA

--- bin/FPCA_sparse.R
+++ bin/FPCA_sparse.R
@@ -1,7 +1,7 @@
 #################
 #SOMLIT MED - 2022
 #Mathilde Couteyen Carpaye 
-#Functional PCA
+#Functional PCA par
 ################
 
 ###working directory###
@@ -11,13 +11,13 @@ setwd("C:/Users/precym-guest/Dropbox/2022_stageM2_COUTEYEN/2022_SOMLIT_MED")
 library(tidyverse)
 library(lubridate)
 library(ggpubr)
-library(fda)
+library(fdapace)
 
 ###source code###
 source("src/my_palette.R")
 
-###save preprocessed data###
-save <- FALSE
+###save preprocess data
+save <- TRUE
 
 ###Preprocessing function for PCA###
 build_ymat <- function(data, var){
@@ -46,17 +46,23 @@ build_ymat <- function(data, var){
   curves_sites <- NULL
   for(s in 1:l){
     one_site <- moyennes %>% filter(ID_SITE==sites[s])
-  for(i in 1:n){
-    an <- one_site %>% filter(YEAR==years[i])
-    for(j in 1:m){
-      an_sp <- an %>% filter(GROUPE==gp[j])
-      if(nrow(an_sp)==12){
-        ymat <- cbind(ymat, an_sp$mean)
-        curves <- c(curves, years[i])
-        spp <- c(spp, gp[j])
-        curves_sites <- c(curves_sites, sites[s])
-    }}}
-  }
+    for(i in 1:n){
+      an <- one_site %>% filter(YEAR==years[i])
+      for(j in 1:m){
+        an_sp <- an %>% filter(GROUPE==gp[j])
+        my_month <- unique(an_sp$MONTH)
+        my_means <- NULL
+        for(z in 1:12){
+          my_mean <- ifelse(z %in% my_month, an_sp$mean[z], NA)
+          my_means <- rbind(my_means, my_mean)
+        }
+        if(sum(is.na(my_means))!=12){
+          ymat <- cbind(ymat, my_means)
+          curves <- c(curves, years[i])
+          spp <- c(spp, gp[j])
+          curves_sites <- c(curves_sites, sites[s])
+        }}
+    }}
   all_info <- data.frame("ANNEE"=curves, "SITE"=curves_sites, "GROUPE"=spp)
   return(list('ymat'=ymat, 'all_info'=all_info))
 }
@@ -74,8 +80,8 @@ my_fpca <- function(y_mat, all_info, variable, sp=NULL, year_lab=FALSE, nutri=FA
   groupe <- 'Groupe'
   if(nutri==TRUE){
     sp_name <- data.frame(code=c("NH4", "NO3", "NO2", "PO4", "SIOH4"),
-                                        name=c("Amonium", "Nitrate", "Nitrite",
-                                               "Phosphate", "Silice"))
+                          name=c("Amonium", "Nitrate", "Nitrite",
+                                 "Phosphate", "Silice"))
     groupe <- 'Nutriment'
   }
   y_mat <- as.matrix(y_mat)
@@ -86,12 +92,9 @@ my_fpca <- function(y_mat, all_info, variable, sp=NULL, year_lab=FALSE, nutri=FA
   argvals <- matrix(arg_time, nrow=n_points, ncol=n_curves)
   #normalisation
   if(norm==TRUE){
-  sum_mat <- apply(y_mat, 2, sum)
-  y_mat <- sweep(y_mat, 2, sum_mat, FUN="/")
+    sum_mat <- apply(y_mat, 2, sum, na.rm=TRUE)
+    y_mat <- sweep(y_mat, 2, sum_mat, FUN="/")
   }
-  #create basis
-  basis <- create.bspline.basis(c(1, 12), breaks=arg_time, norder=4)
-  l_use <- 1e-4 #lambda
   #select sp
   if(is.null(sp)==FALSE){
     ind <- which(all_info$GROUPE %in% sp)
@@ -100,63 +103,66 @@ my_fpca <- function(y_mat, all_info, variable, sp=NULL, year_lab=FALSE, nutri=FA
     all_info <- all_info[ind,]
     sp_name <- sp_name %>% filter(code %in% sp)
   }
-  W.obj <- Data2fd(argvals = argvals, y = y_mat, basisobj = basis, lambda = l_use)
   ##FACP
-  facp <- pca.fd(fdobj = W.obj, nharm=5)
-  ##Components indertia
-  pc1_pi <- round(facp$values[1]/sum(facp$values)*100, 2)
-  pc2_pi <- round(facp$values[2]/sum(facp$values)*100, 2)
-  pc3_pi <- round(facp$values[3]/sum(facp$values)*100, 2)
+  facp <- FPCA(data.frame(y_mat), data.frame(argvals),
+              optns=list(useBinnedData='OFF',
+                         methodMuCovEst='smooth',
+                         userBwCov=3,
+                         userBwMu=2,
+                         kernel="quar",
+                         nRegGrid=100,
+                         dataType="Sparse",
+                         methodXi='CE'
+              ))
+
+  ##Components inertia
+  pc1_pi <- round(facp$lambda[1]/sum(facp$lambda)*100, 2)
+  pc2_pi <- round(facp$lambda[2]/sum(facp$lambda)*100, 2)
+  pc3_pi <- round(facp$lambda[3]/sum(facp$lambda)*100, 2)
   ##PC as mean perturbation
-  pert_1p <- fd(basisobj=basis)
-  pert_1n <- fd(basisobj=basis)
-  pert_2p <- fd(basisobj=basis)
-  pert_2n <- fd(basisobj=basis)
-  pert_3p <- fd(basisobj=basis)
-  pert_3n <- fd(basisobj=basis)
-  xx <- seq(1, 12, 0.2)
-  mean <- mean.fd(W.obj)
-  pc1 <- matrix(facp$harmonics$coefs[,1], ncol=1)
-  pert_1p$coefs <- mean$coefs + sqrt(facp$values[1]) * pc1
-  pert_1n$coefs <- mean$coefs - sqrt(facp$values[1]) * pc1
-  df_mean <- data.frame(x=xx, yhat = predict(mean, newdata=xx))
-  df_pcp1 <- data.frame(x=xx, yhat = predict(pert_1p, newdata=xx))
-  df_pcn1 <- data.frame(x=xx, yhat = predict(pert_1n, newdata=xx))
-  pc2 <- matrix(facp$harmonics$coefs[,2], ncol=1)
-  pert_2p$coefs <- mean$coefs + sqrt(facp$values[2]) * pc2
-  pert_2n$coefs <- mean$coefs - sqrt(facp$values[2]) * pc2
-  df_pcp2 <- data.frame(x=xx, yhat = predict(pert_2p, newdata=xx))
-  df_pcn2 <- data.frame(x=xx, yhat = predict(pert_2n, newdata=xx))
-  pc3 <- matrix(facp$harmonics$coefs[,3], ncol=1)
-  pert_3p$coefs <- mean$coefs + sqrt(facp$values[3]) * pc3
-  pert_3n$coefs <- mean$coefs - sqrt(facp$values[3]) * pc3
-  df_pcp3 <- data.frame(x=xx, yhat = predict(pert_3p, newdata=xx))
-  df_pcn3 <- data.frame(x=xx, yhat = predict(pert_3n, newdata=xx))
+  xx <- facp$workGrid
+  mean <- facp$mu
+  pc1 <- facp$phi[,1]
+  pert_1p <- mean + sqrt(facp$lambda[1]) * pc1
+  pert_1n <- mean - sqrt(facp$lambda[1]) * pc1
+  df_mean <- data.frame(x=xx, yhat = mean)
+  df_pcp1 <- data.frame(x=xx, yhat = pert_1p)
+  df_pcn1 <- data.frame(x=xx, yhat = pert_1n)
+  pc2 <- facp$phi[,2]
+  pert_2p <- mean + sqrt(facp$lambda[2]) * pc2
+  pert_2n <- mean - sqrt(facp$lambda[2]) * pc2
+  df_pcp2 <- data.frame(x=xx, yhat = pert_2p)
+  df_pcn2 <- data.frame(x=xx, yhat = pert_2n)
+  pc3 <- facp$phi[,3]
+  pert_3p <- mean + sqrt(facp$lambda[3]) * pc3
+  pert_3n <- mean - sqrt(facp$lambda[3]) * pc3
+  df_pcp3 <- data.frame(x=xx, yhat = pert_3p)
+  df_pcn3 <- data.frame(x=xx, yhat = pert_3n)
   mois <- c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep",
             "Oct", "Nov", "Dec")
-  A <- ggplot() + geom_path(data=df_mean, aes(xx, mean), cex=1) +
-    geom_point(data=df_pcp1, aes(xx, mean), cex=1, col=my_palette[1], shape=3) +
-    geom_path(data=df_pcn1, aes(xx, mean), cex=1, col=my_palette[2], linetype='dashed') +
-    theme_light() + ylab(variable) + xlab('Temps') +
+  A <- ggplot() + geom_path(data=df_mean, aes(xx, yhat), cex=1) +
+    geom_point(data=df_pcp1, aes(xx, yhat), cex=1, col=my_palette[1], shape=3) +
+    geom_path(data=df_pcn1, aes(xx, yhat), cex=1, col=my_palette[2], linetype='dashed') +
+    theme_light() + ylab('variable') + xlab('Temps') +
     scale_x_continuous(breaks = 1:12, labels=mois) +
     ggtitle(paste('PC1', pc1_pi, '%'))
-  B <- ggplot() + geom_path(data=df_mean, aes(xx, mean), cex=1) +
-    geom_point(data=df_pcp2, aes(xx, mean), cex=1, col=my_palette[1], shape=3) +
-    geom_path(data=df_pcn2, aes(xx, mean), cex=1, col=my_palette[2], linetype='dashed') +
-    theme_light() + ylab(variable) + xlab('Temps') +
+  B <- ggplot() + geom_path(data=df_mean, aes(xx, yhat), cex=1) +
+    geom_point(data=df_pcp2, aes(xx, yhat), cex=1, col=my_palette[1], shape=3) +
+    geom_path(data=df_pcn2, aes(xx, yhat), cex=1, col=my_palette[2], linetype='dashed') +
+    theme_light() + ylab('variable') + xlab('Temps') +
     scale_x_continuous(breaks = 1:12, labels=mois) +
     ggtitle(paste('PC2', pc2_pi, '%'))
-  C <- ggplot() + geom_path(data=df_mean, aes(xx, mean), cex=1) +
-    geom_point(data=df_pcp3, aes(xx, mean), cex=1, col=my_palette[1], shape=3) +
-    geom_path(data=df_pcn3, aes(xx, mean), cex=1, col=my_palette[2], linetype='dashed') +
-    theme_light() + ylab(variable) + xlab('Temps') +
+  C <- ggplot() + geom_path(data=df_mean, aes(xx, yhat), cex=1) +
+    geom_point(data=df_pcp3, aes(xx, yhat), cex=1, col=my_palette[1], shape=3) +
+    geom_path(data=df_pcn3, aes(xx, yhat), cex=1, col=my_palette[2], linetype='dashed') +
+    theme_light() + ylab('variable') + xlab('Temps') +
     scale_x_continuous(breaks = 1:12, labels=mois) +
     ggtitle(paste('PC3', pc3_pi, '%'))
   plot_pert <- ggarrange(A,B, C, ncol=1, nrow=3)
   ##Components scores
-  df_pc <- data.frame('PC1'=facp$scores[,1], 
-                      'PC2'=facp$scores[,2], 
-                      'PC3'=facp$scores[,3])
+  df_pc <- data.frame('PC1'=facp$xiEst[,1], 
+                      'PC2'=facp$xiEst[,2], 
+                      'PC3'=facp$xiEst[,3])
   my_df <- cbind(df_pc, all_info)
   my_df$SITE <- factor(my_df$SITE, 
                        labels=c('Banyuls', 'Marseille', 'Villefranche'))
@@ -183,7 +189,7 @@ my_fpca <- function(y_mat, all_info, variable, sp=NULL, year_lab=FALSE, nutri=FA
   }
   if(length(sp)!=1){
     plot_score <- plot_score +
-      geom_point(aes(PC1, PC2, col=SITE, shape=GROUPE), size=2) +
+      geom_point(aes(PC1, PC2, col=factor(SITE), shape=GROUPE), size=2) 
       scale_shape_discrete(name=groupe) 
   }
   if(year_lab==TRUE){
@@ -203,32 +209,35 @@ data_diff <- read.csv("data/PICONANO_DIFF.csv")
 ###save preprocessed data###
 if(save==TRUE){
   export <- build_ymat(data_ab, 'ABONDANCE')
-  write.csv(export$ymat, 'results/FPCA_YMAT_AB.csv', row.names=FALSE)
-  write.csv(export$all_info, 'results/FPCA_INFO_AB.csv', row.names=FALSE)
+  write.csv(export$ymat, 'results/fpca_ymat_ab.csv', row.names=FALSE)
+  write.csv(export$all_info, 'results/fpca_info_ab.csv', row.names=FALSE)
   export <- build_ymat(data_nutri, 'CONCENTRATION')
-  write.csv(export$ymat, 'results/FPCA_YMAT_NUTRI.csv', row.names=FALSE)
-  write.csv(export$all_info, 'results/FPCA_INFO_NUTRI.csv', row.names=FALSE)
+  write.csv(export$ymat, 'results/fpca_ymat_nutri.csv', row.names=FALSE)
+  write.csv(export$all_info, 'results/fpca_info_nutri.csv', row.names=FALSE)
   export <- build_ymat(data_diff, 'DIFFUSION')
-  write.csv(export$ymat, 'results/FPCA_YMAT_DIFF.csv', row.names=FALSE)
-  write.csv(export$all_info, 'results/FPCA_INFO_DIFF.csv', row.names=FALSE)
+  write.csv(export$ymat, 'results/fpca_ymat_diff.csv', row.names=FALSE)
+  write.csv(export$all_info, 'results/fpca_info_diff.csv', row.names=FALSE)
 }
 
 ###load preprocess data###
-ymat_ab <- read.csv('results/FPCA_YMAT_AB.csv')
-info_ab <- read.csv('results/FPCA_INFO_AB.csv')
-ymat_nutri <- read.csv('results/FPCA_YMAT_NUTRI.csv')
-info_nutri <- read.csv('results/FPCA_INFO_NUTRI.csv')
-ymat_diff <- read.csv('results/FPCA_YMAT_DIFF.csv')
-info_diff <- read.csv('results/FPCA_INFO_DIFF.csv')
+ymat_ab <- read.csv('results/fpca_ymat_ab.csv')
+info_ab <- read.csv('results/fpca_info_ab.csv')
+ymat_nutri <- read.csv('results/fpca_ymat_nutri.csv')
+info_nutri <- read.csv('results/fpca_info_nutri.csv')
+ymat_diff <- read.csv('results/fpca_ymat_diff.csv')
+info_diff <- read.csv('results/fpca_info_diff.csv')
 
 ###fpca on abundance###
-out1 <- which(info_ab$GROUPE=='PROC' & 
-                    info_ab$SITE==12 & 
-                    info_ab$ANNEE==2016)
-out2 <- which(info_ab$GROUPE=='PICOEC' & 
+out1 <- which(info_ab$ANNEE==2021)
+out2 <- which(info_ab$SITE==12 & 
+                info_ab$ANNEE==2019)
+out3 <- which(info_ab$GROUPE=='PROC' & 
+                info_ab$SITE==12 & 
+                info_ab$ANNEE==2016)
+out4 <- which(info_ab$GROUPE=='PICOEC' & 
                 info_ab$SITE==11 & 
                 info_ab$ANNEE==2013)
-outs <- c(out1, out2)
+outs <- c(out1, out2, out3, out4)
 ymat_ab <- ymat_ab[,-outs]
 info_ab <- info_ab[-outs,]
 fpca_ab <- my_fpca(ymat_ab, info_ab, 'Abondance')
@@ -237,10 +246,10 @@ fpca_pro <- my_fpca(ymat_ab, info_ab, 'Abondance', sp='PROC', year_lab=TRUE, nor
 fpca_syn <- my_fpca(ymat_ab, info_ab, 'Abondance', sp='SYNC', year_lab=TRUE, norm=FALSE)
 fpca_picoe <- my_fpca(ymat_ab, info_ab, 'Abondance', sp='PICOEC', year_lab=TRUE, norm=FALSE)
 fpca_nanoe <- my_fpca(ymat_ab, info_ab, 'Abondance', sp='NANOEC', year_lab=TRUE, norm=FALSE)
-fpca_other <- my_fpca(ymat_ab, info_ab, 'Abondance', sp=c('SYNC', 'PICOEC', 'NANOEC'))
+fpca_other <- my_fpca(ymat_ab, info_ab, 'Abondance', sp=c('SYNC', 'PICOEC', 'NANOEC'), year_lab=TRUE)
 fpca_picoe
 fpca_cry
-fpca_other
+
 
 
 ###fpca on nutrients###
@@ -248,20 +257,14 @@ out1 <- which(info_nutri$GROUPE=='NH4' &
                 info_nutri$SITE==11 & 
                 info_nutri$ANNEE==2018)
 out2 <- which(info_nutri$GROUPE=='PO4' & 
-                info_nutri$SITE==12 & 
-                info_nutri$ANNEE==2014)
-out3 <- which(info_nutri$GROUPE=='PO4' & 
                 info_nutri$SITE==11 & 
                 info_nutri$ANNEE==2014)
 outs <- c(out1, out2, out3)
 ymat_nutri <- ymat_nutri[,-outs]
 info_nutri <- info_nutri[-outs,]
 fpca_nutri <- my_fpca(ymat_nutri, info_nutri, 'Concentration', nutri=TRUE)
-fpca_N <- my_fpca(ymat_nutri, info_nutri, 'Concentration', 
-                      sp=c('NH4', 'NO3', 'NO2'), 
-                      nutri=TRUE)
 fpca_NH4 <- my_fpca(ymat_nutri, info_nutri, 'Concentration', sp='NH4', 
-                  year_lab=TRUE, nutri=TRUE, norm=FALSE)
+                    year_lab=TRUE, nutri=TRUE, norm=FALSE)
 fpca_NO3 <- my_fpca(ymat_nutri, info_nutri, 'Concentration', sp='NO3', 
                     year_lab=TRUE, nutri=TRUE, norm=FALSE)
 fpca_NO2 <- my_fpca(ymat_nutri, info_nutri, 'Concentration', sp='NO2', 
@@ -275,18 +278,10 @@ fpca_NO3
 fpca_P
 
 ###fpca on diffusion###
-out1 <- which(info_diff$GROUPE=='PICOESSC' & 
-                info_diff$SITE==11 & 
-                info_diff$ANNEE==2019)
-out2 <- which(info_diff$GROUPE=='NANOESSC' & 
-                info_diff$SITE==11 & 
-                info_diff$ANNEE==2013)
-outs <- c(out1, out2)
-ymat_diff <- ymat_diff[,-outs]
-info_diff <- info_diff[-outs,]
 fpca_diff <- my_fpca(ymat_diff, info_diff, 'Diffusion')
 fpca_dcry <- my_fpca(ymat_diff, info_diff, 'Diffusion', sp='CRYSSC', year_lab=TRUE, norm=FALSE)
+fpca_dpro <- my_fpca(ymat_diff, info_diff, 'Diffusion', sp='PROSSC', year_lab=TRUE, norm=FALSE)
 fpca_dsyn <- my_fpca(ymat_diff, info_diff, 'Diffusion', sp='SYNSSC', year_lab=TRUE, norm=FALSE)
 fpca_dpicoe <- my_fpca(ymat_diff, info_diff, 'Diffusion', sp='PICOESSC', year_lab=TRUE, norm=FALSE)
 fpca_dnanoe <- my_fpca(ymat_diff, info_diff, 'Diffusion', sp='NANOESSC', year_lab=TRUE, norm=FALSE)
-
+fpca_dnanoe
