fc802e4ad657e9e4bf1b0e2566ecebb635834686
MCouteyen <m.couteyen_carpaye@ecologie.re>
2022-05-04 15:53:47 +0200

    Use lda from MASS

--- bin/FDA.R
+++ bin/FDA.R
@@ -9,7 +9,7 @@ setwd("C:/Users/precym-guest/Dropbox/2022_stageM2_COUTEYEN/2022_SOMLIT_MED")
 
 ###packages### 
 library(tidyverse)
-library(FactoMineR)
+library(MASS)
 library(missMDA)
 library(ggforce)
 
@@ -19,112 +19,86 @@ source("src/my_palette.R")
 ###load data###
 data_ab <- read.csv('results/PC_AB.csv')
 data_nutri <- read.csv('results/PC_NUTRI.csv')
+data_diff <- read.csv('results/PC_DIFF.csv')
 
 ###fda plots function###
 my_fda <- function(data, variable, nutri=FALSE){
   mat <- data[,-(1:2)]
   ##NA treatment
   imputed <- imputePCA(mat, ncp=2)
-  acp <- PCA(imputed$completeObs, ncp=2, graph=FALSE)
-  p_axe1 <- round(acp$eig[1,2], 2)
-  p_axe2 <- round(acp$eig[2,2], 2)
-  my_df <- data.frame("SITE"=data[,2], acp$ind$coord)
-  my_df %>% ggplot() + geom_point(aes(Dim.1, Dim.2, col=factor(SITE))) +
+  #fda
+  input <- data.frame("SITE"=data[,2], imputed$completeObs)
+  var <- lda(SITE~., input)
+  ind <- predict(var, data=input)
+  p_axe1 <- round(prop.table(var$svd^2)[1]*100, 2)
+  p_axe2 <- round(prop.table(var$svd^2)[2]*100, 2)
+  my_df <- data.frame("ANNEE"=data[,1],"SITE"=data[,2], ind$x)
+  my_df %>% ggplot() + geom_point(aes(LD1, LD2, col=factor(SITE))) +
     geom_vline(xintercept=0, linetype='dashed') +
     geom_hline(yintercept=0, linetype='dashed') +
     theme_light()
   Xacp1 <- my_df %>% filter(SITE=="Banyuls")
   Xacp2 <- my_df %>% filter(SITE=="Marseille")
   Xacp3 <- my_df %>% filter(SITE=="Villefranche")
-  g1 <- apply(Xacp1[,2:3], 2, mean)
-  g2 <- apply(Xacp2[,2:3], 2, mean)
-  g3 <- apply(Xacp3[,2:3], 2, mean)
+  g1 <- apply(Xacp1[,3:4], 2, mean)
+  g2 <- apply(Xacp2[,3:4], 2, mean)
+  g3 <- apply(Xacp3[,3:4], 2, mean)
   grav_center <- rbind(g1, g2, g3)
   grav_center <- data.frame("SITE"=c('Banyuls', 'Marseille', 'Villefranche'),
                             grav_center)
   ban <- my_df %>% filter(SITE=='Banyuls')
   mar <- my_df %>% filter(SITE=='Marseille')
   vil <- my_df %>% filter(SITE=='Villefranche')
-  acp_plot <- my_df %>% ggplot() + geom_point(aes(Dim.1, Dim.2, col=factor(SITE))) +
+  acp_plot <- my_df %>% ggplot() + geom_point(aes(LD1, LD2, col=factor(SITE))) +
     geom_vline(xintercept=0, linetype='dashed') +
     geom_hline(yintercept=0, linetype='dashed') +
     theme_light() +
-    geom_point(data=grav_center, aes(Dim.1, Dim.2, col=factor(SITE)), shape=3) +
-    geom_segment(data=ban, aes(g1[1], g1[2], xend=Dim.1, yend=Dim.2, col=factor(SITE))) +
-    geom_segment(data=mar, aes(g2[1], g2[2], xend=Dim.1, yend=Dim.2, col=factor(SITE))) +
-    geom_segment(data=vil, aes(g3[1], g3[2], xend=Dim.1, yend=Dim.2, col=factor(SITE))) +
+    geom_point(data=grav_center, aes(LD1, LD2, col=factor(SITE)), shape=3) +
+    geom_text(data=grav_center, aes(LD1, LD2, col=factor(SITE), label=SITE),
+              fontface='bold', size=4.5, vjust=1, hjust=1) +
+    geom_segment(data=ban, aes(g1[1], g1[2], xend=LD1, yend=LD2, col=factor(SITE))) +
+    geom_segment(data=mar, aes(g2[1], g2[2], xend=LD1, yend=LD2, col=factor(SITE))) +
+    geom_segment(data=vil, aes(g3[1], g3[2], xend=LD1, yend=LD2, col=factor(SITE))) +
     scale_color_manual(values=my_palette[2:4], name='Site') +
     xlab(paste('Axe 1 ', p_axe1, '%')) + ylab(paste('Axe 2 ', p_axe2, '%')) + ggtitle(variable)+
     theme(legend.text = element_text(size=13),
+          legend.position='none',
           legend.title = element_text(size=14),
           axis.text=element_text(size=12),
           axis.title=element_text(size=14))
-  coord <- data.frame('VAR'=row.names(acp$var$coord), acp$var$coord)
+  coord <- data.frame('VAR'=row.names(var$scaling), var$scaling)
   coord <- coord %>% separate('VAR', c('PC', 'GROUPE'), '_')
-  contrib <-  data.frame('VAR'=row.names(acp$var$coord), acp$var$contrib) 
-  contrib <- contrib %>% separate('VAR', c('PC', 'GROUPE'), '_')
   leg_name <- 'Groupe'
   if(nutri==TRUE){
-    contrib$GROUPE <- factor(contrib$GROUPE, 
-                             labels=c('NH4', 'NO3', 'NO2', 'PO4', 'SiOH(4)'))
-    leg_name <- 'Nutriment'
+   leg_name <- 'Nutriment'
   }
   var_plot <- coord %>% ggplot() + 
-    geom_segment(aes(0, 0, xend=Dim.1, yend=Dim.2)) +
+    geom_segment(aes(0, 0, xend=LD1, yend=LD2)) +
     geom_vline(xintercept=0, linetype='dashed') +
     geom_hline(yintercept=0, linetype='dashed') +
-    geom_circle(aes(x0 = 0, y0 = 0, r = 1), inherit.aes = FALSE) +
-    geom_label(aes(Dim.1, Dim.2, label=PC, fill=GROUPE), 
-               color='white', size=4.5) +
+    geom_circle(aes(x0 = 0, y0 = 0, r = 1), inherit.aes = FALSE, 
+                size=.2, linetype='dashed', alpha=.6) +
+    geom_label(aes(LD1, LD2, label=PC, fill=GROUPE),
+               size=4.5, fontface='bold', alpha=.8, col='white') +
     theme_light() + xlab(paste('Axe 1 ', p_axe1, '%')) + ylab(paste('Axe 2 ', p_axe2, '%')) + ggtitle(variable) +
     scale_fill_manual(values=my_palette, name=leg_name)
-  contrib <- contrib %>% unite('VAR', c('PC', 'GROUPE'), sep='_', remove=FALSE)
-  axe_un <- contrib %>% arrange(desc(Dim.1)) %>% 
-    mutate(VAR=factor(VAR, levels=VAR)) %>%
-    ggplot() + theme_minimal() + xlab('Variable') +
-    geom_bar(aes(VAR, Dim.1, fill=GROUPE), 
-             stat='identity', width=.6, alpha=.8) +
-    scale_fill_manual(values=my_palette, name=leg_name) +
-    theme(axis.text.x = element_text(size=9, angle=45, hjust=1),
-          legend.text = element_text(size=13),
-          legend.title = element_text(size=14),
-          axis.text=element_text(size=12),
-          axis.title=element_text(size=14))
-  axe_deux <- contrib %>% arrange(desc(Dim.2)) %>% 
-    mutate(VAR=factor(VAR, levels=VAR)) %>%
-    ggplot() + theme_minimal() + xlab('Variable') +
-    geom_bar(aes(VAR, Dim.2, fill=GROUPE), 
-             stat='identity', width=.6, alpha=.8) +
-    scale_fill_manual(values=my_palette, name=leg_name) +
-    theme(axis.text.x = element_text(size=9, angle=45, hjust=1),
-          legend.text = element_text(size=13),
-          legend.title = element_text(size=14),
-          axis.text=element_text(size=12),
-          axis.title=element_text(size=14))
-  if(nutri==FALSE){
-    axe_un <- axe_un + scale_x_discrete(labels=abbreviate)
-    axe_deux <- axe_deux + scale_x_discrete(labels=abbreviate) 
-  }
-  contrib_plot <- ggarrange(NULL, axe_un, NULL, axe_deux, 
-                            ncol=1, nrow=4,
-                            heights=c(0.1, 1, 0.1, 1),
-                            labels=c('', 'Axe 1', '', 'Axe 2'),
-                            font.label=list(size=12, face='plain'),
-                            common.legend=TRUE, legend='bottom',
-                            vjust=0.6)
-  return(list(acp=acp_plot, var=var_plot, contrib=contrib_plot))
+  return(list(acp=acp_plot, var=var_plot, data=my_df))
 }
 
 ####ABUNDANCE
 my_fda(data_ab, 'Abondance')$acp
 x11()
 my_fda(data_ab, 'Abondance')$var
-x11()
-my_fda(data_ab, 'Abondance')$contrib
 
-####ANUTRIENTS
+####NUTRIENTS
+x11()
 my_fda(data_nutri, 'Nutriments', nutri=TRUE)$acp
 x11()
 my_fda(data_nutri, 'Nutriments', nutri=TRUE)$var
+
+####DIFFUSION
 x11()
-my_fda(data_nutri, 'Nutriments', nutri=TRUE)$contrib
+my_fda(data_diff, 'Diffusion', nutri=TRUE)$acp
+x11()
+my_fda(data_diff, 'Diffusion', nutri=TRUE)$var
+
