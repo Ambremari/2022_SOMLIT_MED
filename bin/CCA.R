c9e272e18e48b458d1dc9daadf131364ba5581ea
Mathilde Couteyen Carpaye <m.couteyen_carpaye@ecologie.re>
2022-05-26 16:51:44 +0200

    add cor plots to cca

--- bin/CCA.R
+++ bin/CCA.R
@@ -6,7 +6,7 @@
 
 
 ###working directory###
-setwd("C:/Users/precym-guest/Dropbox/2022_stageM2_COUTEYEN/2022_SOMLIT_MED")
+setwd("C:/Users/ambre/Dropbox/2022_stageM2_COUTEYEN/2022_SOMLIT_MED")
 
 
 ###load packages###
@@ -25,29 +25,33 @@ diff <- read.csv('results/PC_DIFF.csv')
 
 
 ###CCA FUNCTION ###
-my_cca <- function(data1, data2, variable){
+my_cca <- function(data1, data2, variable, dim1, dim2){
   data <- left_join(data1, data2, by=c('ANNEE', 'SITE'))
   data1 <- data[, 3:7]
   data2 <- data[,18: 22]
   cca <- cc(data2, data1)
-  cor1 <- round(cca$cor[1], 3)
-  cor2 <- round(cca$cor[2], 3)
+  cor1 <- round(cca$cor[dim1], 3)
+  cor2 <- round(cca$cor[dim2], 3)
   CC_X <- as.matrix(data2) %*% cca$xcoef
-  my_df <- data.frame('SITE'=data$SITE, 'CC1_x'=CC_X[,1], 'CC2_x'=CC_X[,2])
-  x_var <- rbind(t(t(cca$scores$corr.X.xscores[,1])),
-                t(t(cca$scores$corr.Y.xscores[,1])))
-  y_var <- rbind(t(t(cca$scores$corr.X.xscores[,2])),
-               t(t(cca$scores$corr.Y.xscores[,2])))
+  my_df <- data.frame('SITE'=data$SITE, 'CC1_x'=CC_X[,dim1], 'CC2_x'=CC_X[,dim2])
+  x_var <- rbind(t(t(cca$scores$corr.X.xscores[,dim1])),
+                t(t(cca$scores$corr.Y.xscores[,dim1])))
+  y_var <- rbind(t(t(cca$scores$corr.X.xscores[,dim2])),
+               t(t(cca$scores$corr.Y.xscores[,dim2])))
   my_var <- c('NH4', 'NO3', 'NO2',  'Si(OH)4', 'PO4', 'CRY', 'SYN', 'PICOE', 'NANOE', 'PRO')
   var_df <- data.frame('VAR'=my_var, 'X'=x_var, 'Y'=y_var)
-  
+  m <- length(cca$cor)
+  cor_df <- data.frame('CC'=1:m, 'cor'=cca$cor)
+  cor_plot <- ggplot() + 
+    geom_bar(data=cor_df, aes(CC, cor), stat='identity') +
+    theme_light() + xlab('Dimension') + ylab('Corrélation canonique')
   ind_plot <- my_df %>% ggplot() + theme_light() +
    geom_point(aes(CC1_x, CC2_x, col=factor(SITE)), size=3.5) +
    scale_color_manual(values=my_palette[2:4], name='Site') +
    geom_vline(xintercept=0, linetype='dashed') +
    geom_hline(yintercept=0, linetype='dashed') +
     ggtitle(variable) +
-   xlab(paste('CC1', cor1)) + ylab(paste('CC2', cor2)) + 
+   xlab(paste('CC', dim1, " ", cor1)) + ylab(paste('CC', dim2, " ", cor2)) + 
     theme(legend.text = element_text(size=18),
           legend.title = element_text(size=18),
           axis.text=element_text(size=16),
@@ -61,18 +65,18 @@ my_cca <- function(data1, data2, variable){
    geom_segment(data=var_df[1:5,], aes(x=0, y=0, xend=X, yend=Y)) +
     geom_vline(xintercept=0, linetype='dashed') +
     geom_hline(yintercept=0, linetype='dashed') +
-    xlab(paste('CC1', cor1)) + ylab(paste('CC2', cor2)) + 
+    xlab(paste('CC', dim1, " ", cor1)) + ylab(paste('CC', dim2, " ", cor2)) + 
     theme(legend.text = element_text(size=16),
           legend.title = element_text(size=16),
           axis.text=element_text(size=16),
           axis.title=element_text(size=18))
-  return(list('var_plot'=var_plot, 'ind_plot'=ind_plot))
+  return(list('var_plot'=var_plot, 'ind_plot'=ind_plot, 'cor_plot'=cor_plot))
 }
 
 ###CCA###
-cca_ab <- my_cca(ab, nutri, 'Abondance')
+cca_ab <- my_cca(ab, nutri, 'Abondance', 1, 2)
 ggarrange(cca_ab$ind_plot, NULL,  cca_ab$var_plot, ncol=3, nrow=1, 
           widths=c(1, .1, 1))
-cca_diff <- my_cca(diff, nutri, 'Diffusion')
+cca_diff <- my_cca(diff, nutri, 'Diffusion', 1, 5)
 ggarrange(cca_diff$ind_plot, NULL,  cca_diff$var_plot, ncol=3, nrow=1, 
           widths=c(1, .1, 1))
